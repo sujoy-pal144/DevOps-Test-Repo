d8e6246e735174e34d887bf086eec42ca4f8da16
Sujoy Pal <sujoypal144@gmail.com>
2024-05-07 15:17:32 +0530

    fix: Add print statements

--- validate_title.py
+++ validate_title.py
@@ -5,10 +5,14 @@ def validate_title(title):
     prefix_pattern = r'^(build|chore|ci|docs|feat|fix|perf|refactor|style|test|sample): .+'
     max_length = 50
     
+    print("The prefix match is:", re.match(prefix_pattern, title, re.IGNORECASE))
+
     if not re.match(prefix_pattern, title, re.IGNORECASE):
         print("PR title must start with one of the following prefixes: build, chore, ci, docs, feat, fix, perf, refactor, style, test, sample", file=sys.stderr)
         sys.exit(1)
-    
+
+    print("Length of the title:", len(title))
+
     if len(title) > max_length:
         print(f"PR title must not exceed {max_length} characters", file=sys.stderr)
         sys.exit(1)
