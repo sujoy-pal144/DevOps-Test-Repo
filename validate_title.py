e9017cf4d0b8ad6bcd07cee10872525cb9096175
Sujoy Pal <sujoypal144@gmail.com>
2024-05-07 14:40:57 +0530

    fix: update print state and remove unnecessary codes

--- validate_title.py
+++ validate_title.py
@@ -2,13 +2,11 @@ import re
 import sys
 
 def validate_title(title):
-    # validation rules
-    # prefix_pattern = r'^(feat|fix|docs|style|refactor|test|chore): .+'
     prefix_pattern = r'^(build|chore|ci|docs|feat|fix|perf|refactor|style|test|sample): .+'
     max_length = 50
     
     if not re.match(prefix_pattern, title, re.IGNORECASE):
-        print("PR title must start with one of the following prefixes: feat, fix, docs, style, refactor, test, chore", file=sys.stderr)
+        print("PR title must start with one of the following prefixes: build, chore, ci, docs, feat, fix, perf, refactor, style, test, sample", file=sys.stderr)
         sys.exit(1)
     
     if len(title) > max_length:
